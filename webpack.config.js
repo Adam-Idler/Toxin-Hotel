e11c5bcf9e105f7f467a9d748aa010027cee2123
Pavel Rakov <pasha_rakov@bk.ru>
2021-06-03 17:00:32 +0700

    Накидал частично страницу Headers & Footers

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   devtool: "eval-source-map",
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: 'scripts/bundle.js',
     assetModuleFilename: 'images/[name][ext]'
   },
   module: {
@@ -50,7 +50,7 @@ module.exports = {
             loader: 'file-loader',
             options: {
               name: `fonts/[name]/[name].[ext]`,
-              publicPath: "./"
+              publicPath: "../"
             }
           }
         ]
@@ -59,10 +59,14 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'styles.css'
+      filename: 'styles/styles.css'
     }),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src') + '/index.pug'
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/pages/headers & footers') + '/index.pug',
+      filename: 'pages/headers&footers.html'
     }),
     new CopyPlugin({
       patterns: [
