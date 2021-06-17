ef2205ec4c66c2db8abbca53e62b49ac80f375cb
Pavel Rakov <pasha_rakov@bk.ru>
2021-06-17 23:38:20 +0700

    Отрефакторил date dropdown, подключил datepicker

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,5 @@
 import { dropdownActivate } from "../components/dropdown/dropdown";
+import { datepickerActivate } from "../components/datepicker/datepicker";
 
 dropdownActivate();
+datepickerActivate();
