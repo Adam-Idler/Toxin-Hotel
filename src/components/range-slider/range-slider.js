12e55cf6f3c9f3f7c72c6eb36ed78b78d221ea7c
Pavel Rakov <pasha_rakov@bk.ru>
2021-07-02 00:51:33 +0700

    Сделал range slider

---
+++ src/components/range-slider/range-slider.js
@@ -0,0 +1,29 @@
+export const rangeSliderActivate = () => {
+
+    const formateValue = (val) => {
+        let str = '',
+            valLength = val.toString().length - 1;
+
+        for (let i = 0; i <= valLength; i++) {
+            if (i % 3 == 0 && i != 0)
+                str += ' ' + val.toString()[valLength - i];
+            else 
+                str += val.toString()[valLength - i];
+        }
+
+        return [...str].reverse().join("");
+    };
+
+    $('.range-slider').slider({
+        animate: "slow",
+        range: true,
+        min: 0,
+        max: 18000, 
+        values: [ 5000, 10000 ],
+        slide : function(event, ui) { 
+            $( ".range-slider__result" ).text( `${formateValue(ui.values[0])}₽ - ${formateValue(ui.values[1])}₽` );        
+        }
+    });
+
+    $( ".range-slider__result" ).text( `${formateValue($(".range-slider").slider("values", 0))}₽ - ${formateValue($(".range-slider").slider("values", 1))}₽` );
+};
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,10 +3,12 @@ import { datepickerActivate } from "../components/datepicker/datepicker";
 import { toggleBtn } from "../components/toggle/toggle";
 import { likeBtn } from "../components/like-button/like-button";
 import { checkboxListActivate } from "../components/checkbox-list/checkbox-list";
+import { rangeSliderActivate } from "../components/range-slider/range-slider";
 import './jquery.datepicker.extension.range.min.js';
 
 dropdownActivate();
 datepickerActivate();
+rangeSliderActivate();
 checkboxListActivate();
 toggleBtn();
 likeBtn();
